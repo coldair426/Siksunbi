538acf8d4cce19a9bd6833acce0a22d8872bfe2e
chanki <coldair426@gmail.com>
2023-03-02 03:57:16 +0900

    active nav category impact edit

--- src/component/MenuNav.js
+++ src/component/MenuNav.js
@@ -1,25 +1,17 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 
 function MenuNav({ category, active, setActive }) {
-  // 활성화된 버튼 스타일 적용
-  useEffect(() => {
-    // 스타일 적용 실행
-    document.querySelectorAll('button')[active].classList.add('nav-active');
-    // 스타일 적용 전 먼저 실행(clean up func)
-    return () => {
-      document.querySelectorAll('button')[active].classList.remove('nav-active');
-    };
-  }, [active]); // active가 변할 때 마다 실행
   return (
     <nav className='nav'>
       <img src='/icon/language.png' alt='translate-icon' />
       <div>
-        <button onClick={() => setActive(0)} className={'nav-item'}>
+        {/* active 클래스 추가하는 jsx */}
+        <button onClick={() => setActive(0)} className={active === 0 ? 'nav-item nav-active' : 'nav-item'}>
           소개
         </button>
         {/* category 별로 button 생성 */}
         {category.map((v, i) => (
-          <button onClick={() => setActive(i + 1)} key={`${v}${i}`} className={'nav-item'}>
+          <button onClick={() => setActive(i + 1)} key={`${v}${i}`} className={active === i + 1 ? 'nav-item nav-active' : 'nav-item'}>
             {v}
           </button>
         ))}
